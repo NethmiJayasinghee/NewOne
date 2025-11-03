2fe2faf954b47cda3af0c8068eceb5097434c6e6
NethmiJayasinghee <ndhananjana98@gmail.com>
2025-11-03 15:02:51 -0800

    Create CustomerController

---
+++ src/main/java/edu/nethmi/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package edu.nethmi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @GetMapping
+    public String getName(){
+        return "Nethmi";
+    }
+}
