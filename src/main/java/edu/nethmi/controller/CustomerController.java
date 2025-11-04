363363840e3c037a85fea554ea154b0bcf711510
NethmiJayasinghee <ndhananjana98@gmail.com>
2025-11-03 17:13:13 -0800

    Added yml file

--- src/main/java/edu/nethmi/controller/CustomerController.java
+++ src/main/java/edu/nethmi/controller/CustomerController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
@@ -28,4 +31,17 @@ public class CustomerController {
                 100000.0
         );
     }
+
+    List<Customer> customerList=new ArrayList<>();
+
+    @GetMapping("/get-all")
+
+    public List<Customer> getAll(){
+        customerList.add(new Customer("C002","Nadun","Kurunegala",75000.0));
+        customerList.add(new Customer("C003","Naduni","Gampaha",750000.0));
+        customerList.add(new Customer("C004","Nadun","Kurunegala",75000.0));
+        customerList.add(new Customer("C005","Nadun","Kurunegala",75000.0));
+        return  customerList;
+    }
+
 }
